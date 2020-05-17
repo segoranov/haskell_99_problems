1ffd93f97cde6c2f5c9f8bb38d929ea71028ede9
Stiliyan Goranov <segoranov@gmail.com>
2020-05-17 21:37:32 +0300

    Implement takeWhile

--- common_functions.hs
+++ common_functions.hs
@@ -12,3 +12,9 @@ zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ [] _ = []
 zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = (f x y): zipWith' f xs ys
+
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' _ [] = []
+takeWhile' pred list@(x:xs)
+    | pred x = x : takeWhile' pred xs
+    | otherwise = []
