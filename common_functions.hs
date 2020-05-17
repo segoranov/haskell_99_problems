bfe686fc648107fa5caf5a1b66ba0a6a019ae76b
Stiliyan Goranov <segoranov@gmail.com>
2020-05-17 21:29:01 +0300

    Implement zipWith

--- common_functions.hs
+++ common_functions.hs
@@ -7,3 +7,8 @@ filter' _ [] = []
 filter' pred (x:xs)
     | pred x = x : filter' pred xs
     | otherwise = filter' pred xs
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = (f x y): zipWith' f xs ys
