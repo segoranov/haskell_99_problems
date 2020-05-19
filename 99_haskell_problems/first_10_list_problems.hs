6710eaf3de3bed6b407c9eeb8ca28de2fe678168
Stiliyan Goranov <segoranov@gmail.com>
2020-05-19 19:11:47 +0300

    Solve problem 10

--- 99_haskell_problems/first_10_list_problems.hs
+++ 99_haskell_problems/first_10_list_problems.hs
@@ -202,3 +202,20 @@ pack''' :: (Eq a) => [a] -> [[a]]
 pack''' [] = []
 pack''' list@(x:xs) = takeWhile (==x) list : pack''' (dropWhile (==x) list)
 
+
+-- Problem 10
+-- (*) Run-length encoding of a list. Use the result of problem P09 to implement the
+-- so-called run-length encoding data compression method. Consecutive duplicates of elements are
+-- encoded as lists (N E) where N is the number of duplicates of the element E.
+
+-- Example:
+
+-- * (encode '(a a a a b c c a a d e e e e))
+-- ((4 A) (1 B) (2 C) (2 A) (1 D)(4 E))
+-- Example in Haskell:
+
+-- λ> encode "aaaabccaadeeee"
+-- [(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
+encode :: (Eq a) => [a] -> [(Int, a)]
+encode = map createEncodedTuple . pack'''
+    where createEncodedTuple xs = (length xs, head xs)
