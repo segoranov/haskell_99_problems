d4f8b83fb96a197d35d624b2a29bf86eca4e1e6b
Stiliyan Goranov <segoranov@gmail.com>
2020-05-19 12:12:10 +0300

    Add examples to problem definitions

--- 99_haskell_problems/first_10_list_problems.hs
+++ 99_haskell_problems/first_10_list_problems.hs
@@ -1,5 +1,12 @@
 -- Problem 1
 -- (*) Find the last element of a list.
+
+-- Example in Haskell:
+-- λ> myLast [1,2,3,4]
+-- 4
+-- λ> myLast ['x','y','z']
+-- 'z'
+
 myLast :: [a] -> a
 myLast [] = error "Called last on empty list."
 myLast [x] = x
@@ -11,6 +18,13 @@ myLast'' = head . reverse
 
 -- Problem 2
 -- (*) Find the last but one element of a list.
+
+-- Example in Haskell:
+-- λ> myButLast [1,2,3,4]
+-- 3
+-- λ> myButLast ['a'..'z']
+-- 'y'
+
 myButLast :: [a] -> a
 myButLast = head . tail . reverse
 
@@ -21,6 +35,13 @@ myButLast' (x:xs) = myButLast xs
 
 -- Problem 3
 -- (*) Find the K'th element of a list. The first element in the list is number 1.
+
+-- Example in Haskell:
+-- λ> elementAt [1,2,3] 2
+-- 2
+-- λ> elementAt "haskell" 5
+-- 'e'
+
 elementAt :: Int -> [a] -> a
 elementAt _ [] = error "Called elementAt on empty list."
 elementAt n list@(x:xs)
@@ -29,3 +50,12 @@ elementAt n list@(x:xs)
     | otherwise = elementAt (n-1) xs
 
 elementAt' n xs = xs !! (n-1)
+
+-- Problem 4
+-- (*) Find the number of elements of a list.
+
+-- Example in Haskell:
+-- λ> myLength [123, 456, 789]
+-- 3
+-- λ> myLength "Hello, world!"
+-- 13
