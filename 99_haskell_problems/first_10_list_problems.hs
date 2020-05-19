3f4d97378e5ab52e6503d984daf0954439f31ce9
Stiliyan Goranov <segoranov@gmail.com>
2020-05-19 18:57:28 +0300

    Implement another solution for pack

--- 99_haskell_problems/first_10_list_problems.hs
+++ 99_haskell_problems/first_10_list_problems.hs
@@ -185,3 +185,10 @@ compress''' xs = foldr (\x acc -> if x == head acc then acc else x : acc) [last 
 pack :: (Eq a) => [a] -> [[a]]
 pack [] = []
 pack xs = foldr (\x acc -> if x `elem` head acc then (x : head acc) : tail acc else [x] : acc) [[last xs]] xs
+
+pack' :: (Eq a) => [a] -> [[a]]
+pack' [] = []
+pack' [x] = [[x]]
+pack' (x:xs) = if x `elem` (head $ pack xs)
+               then (x:(head $ pack xs)):(tail $ pack xs)
+               else [x]:(pack xs)
